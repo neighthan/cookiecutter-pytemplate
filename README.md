4521374695eb460e3c3f79023074819aa2934e29
Nathan Hunt <neighthan.hunt@gmail.com>
2019-02-20 14:25:32 -0500

    Fix name of .cookiecutter.json (remove "_").

--- README.md
+++ README.md
@@ -13,7 +13,7 @@ See the `demo-project` directory for an example project created from this templa
 
 ```{directory_structure}
 demo-project
-├── .cookie_cutter.json
+├── .cookiecutter.json
 ├── demo_project
 │   └── __init__.py
 ├── .gitignore
--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -11,13 +11,13 @@ if include_invoke_tasks == "n":
 # TODO - is there any better way to save all of this without needing to
 # update the dict here each time cookiecutter.json is updated?
 cookiecutter_dict = {
-  "project_name": "{{cookiecutter.project_name}}",
-  "description": "{{cookiecutter.description}}",
-  "full_name": "{{cookiecutter.full_name}}",
-  "email": "{{cookiecutter.email}}",
-  "github_username": "{{cookiecutter.github_username}}",
-  "python_version": "{{cookiecutter.python_version}}",
-  "include_invoke_tasks": "{{cookiecutter.include_invoke_tasks}}"
+    "project_name": "{{cookiecutter.project_name}}",
+    "description": "{{cookiecutter.description}}",
+    "full_name": "{{cookiecutter.full_name}}",
+    "email": "{{cookiecutter.email}}",
+    "github_username": "{{cookiecutter.github_username}}",
+    "python_version": "{{cookiecutter.python_version}}",
+    "include_invoke_tasks": "{{cookiecutter.include_invoke_tasks}}",
 }
 
-Path("./.cookie_cutter.json").write_text(json.dumps(cookiecutter_dict))
+Path("./.cookiecutter.json").write_text(json.dumps(cookiecutter_dict))
