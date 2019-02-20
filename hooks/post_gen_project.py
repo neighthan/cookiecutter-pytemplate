24e99bbc6eb2b086ed2f4707df0d5f6560d20258
Nathan Hunt <neighthan.hunt@gmail.com>
2019-02-20 13:38:26 -0500

    Create .cookiecutter.json with all template values.

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,3 +1,4 @@
+import json
 from pathlib import Path
 
 project_name = "{{cookiecutter.project_name}}"
@@ -6,3 +7,17 @@ Path(project_name).rename(project_name.replace("-", "_"))
 include_invoke_tasks = "{{cookiecutter.include_invoke_tasks}}"
 if include_invoke_tasks == "n":
     Path("./tasks.py").unlink()
+
+# TODO - is there any better way to save all of this without needing to
+# update the dict here each time cookiecutter.json is updated?
+cookiecutter_dict = {
+  "project_name": "{{cookiecutter.project_name}}",
+  "description": "{{cookiecutter.description}}",
+  "full_name": "{{cookiecutter.full_name}}",
+  "email": "{{cookiecutter.email}}",
+  "github_username": "{{cookiecutter.github_username}}",
+  "python_version": "{{cookiecutter.python_version}}",
+  "include_invoke_tasks": "{{cookiecutter.include_invoke_tasks}}"
+}
+
+Path("./.cookie_cutter.json").write_text(json.dumps(cookiecutter_dict))
