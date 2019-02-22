f2307b7c957a4a47523f869908bc54228293e1d5
Nathan Hunt <neighthan.hunt@gmail.com>
2019-02-22 11:38:13 -0500

    Fix test to put `tasks` at the start of PATH.
    Otherwise, I was getting errors from trying to import some other `tasks` file on my PATH; no namespacing is problematic.

--- tests/test_tasks.py
+++ tests/test_tasks.py
@@ -2,8 +2,9 @@ import sys
 from pathlib import Path
 import pytest
 
+# this is hacky, but I don't want to actually _install_ the template
 tasks_dir = Path(__file__).parents[1] / "{{cookiecutter.project_name}}"
-sys.path.append(str(tasks_dir))
+sys.path = [str(tasks_dir)] + sys.path
 
 from tasks import _get_next_dev_num
 
@@ -14,6 +15,8 @@ class TestBuild:
         # this test only works if no capturing of stdout is done, but
         # capfd.disabled() and capsys.disabled() don't fix it; need
         # to run pytest -s for it to work
+        # TODO - use the mark shown above to filter out whether this test runs based on
+        # if -s has been given
 
         project_name = "gpu-utils"
         version = "0.2.1"
